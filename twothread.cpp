ed9ceb90bcbc29fdddacfa0be20016d983f29fd2
Guilherme Amaral <guifamaral27@gmail.com>
2024-06-16 01:30:03 +0200

    T pop function and declaration added

--- twothread.cpp
+++ twothread.cpp
@@ -13,9 +13,23 @@ class threadsafequeue
     public:
         void push(T value)
         {
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(mutex);
 
-            queue_.push(std::move(value));
-            cond_var_.notify_one();
+            queue.push(std::move(value));
+            condvar.notify_one();
         }
+
+        T pop()
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            condvar.wait(lock, [this] { return !queue.empty(); });
+            T value = std::move(queue.front());
+            queue.pop();
+            return value;
+        }
+
+    private:
+        std::queue<T> queue;
+        std::mutex mutex;
+        std::condition_variable condvar;
 };
